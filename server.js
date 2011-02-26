0d5a8e72a20f4c6b6ec24fcbc422144b300288bd
ique <ique@3h.se>
2011-02-26 13:31:15 +0900

    Reworking the application to use express and jade template rendering.

--- server.js
+++ server.js
@@ -1,49 +1,44 @@
-/**
- * Important note: this application is not suitable for benchmarks!
- */
-
 var http = require('http')
   , url = require('url')
   , fs = require('fs')
   , io = require('socket.io')
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
-  , server;
+  , express = require('express')
+  , app = express.createServer();
     
-server = http.createServer(function(req, res){
-  // your normal server code
-  var path = url.parse(req.url).pathname;
-  switch (path){
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Welcome! Try the <a href="/chat.html">chat</a> example.</h1>');
-      res.end();
-      break;
-      
-    case '/json.js':
-    case '/chat.html':
-      fs.readFile(__dirname + path, function(err, data){
-        if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
-        res.write(data, 'utf8');
-        res.end();
-      });
-      break;
-      
-    default: send404(res);
-  }
-}),
+app.configure(function(){
+  app.use(express.staticProvider(__dirname + '/public'));
+  app.set('view options', {
+      layout: false
+  });
+});
 
-send404 = function(res){
-  res.writeHead(404);
-  res.write('404');
-  res.end();
-};
+app.get('/',function(req,res){
+  res.render('index.jade');
+});
 
-server.listen(8080);
+app.get('/poop',function(req,res){
+  poop();
+});
+
+app.get('*', function(req,res){
+  res.render('404.jade', { status: 404 });
+});
+
+app.error(function(err, req, res) {
+  res.render('500.jade', {
+    status: 500,
+    locals: {
+      error: err
+    } 
+  });
+});
+
+app.listen(8080);
 
 // socket.io, I choose you
 // simplest chat application evar
-var io = io.listen(server)
+var io = io.listen(app)
   , buffer = [];
   
 io.on('connection', function(client){
